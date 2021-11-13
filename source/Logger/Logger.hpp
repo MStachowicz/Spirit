739a843abe431b9534392b4da27dc347973c9159
Michal S <michalstachowicz.95@gmail.com>
2021-11-13 23:26:33 +0000

    Disabling/enabling logging based on configuration.

--- source/Logger/Logger.hpp
+++ source/Logger/Logger.hpp
@@ -11,9 +11,19 @@ private:
     static std::shared_ptr<spdlog::logger> sLogger;
 };
 
+
+#ifdef ZEPHYR_CONFIG_DEBUG
 // Logging macros
 #define LOG_TRACE(...)      Logger::GetLogger()->trace(__VA_ARGS__)
 #define LOG_INFO(...)       Logger::GetLogger()->info(__VA_ARGS__)
 #define LOG_WARN(...)       Logger::GetLogger()->warn(__VA_ARGS__)
 #define LOG_ERROR(...)      Logger::GetLogger()->error(__VA_ARGS__)
 #define LOG_CRITICAL(...)   Logger::GetLogger()->critical(__VA_ARGS__)
+#else if ZEPHYR_CONFIG_RELEASE
+// Disable logging for release builds
+#define LOG_TRACE(...)      (void)0   
+#define LOG_INFO(...)       (void)0
+#define LOG_WARN(...)       (void)0
+#define LOG_ERROR(...)      (void)0
+#define LOG_CRITICAL(...)   (void)0
+#endif
