6f6d5029e15b347ff0efb5a1a66ba20744e3d620
Michal S <michalstachowicz.95@gmail.com>
2021-12-14 22:30:41 +0000

    Added example usage of current rendering features. Rendering 4 'Mesh' objects, 2 Triangles, 2 squares. Demos EBO index drawing, multiple textures, colours.

--- source/Renderer/Renderer.cpp
+++ source/Renderer/Renderer.cpp
@@ -10,8 +10,87 @@ bool Renderer::initialise()
 		LOG_CRITICAL("Failed to initalise the graphics context for Renderer to use.");
 		return false;
 	}
-	else
-		return true;
+
+	{ // TRIANGLE - COLOUR ATTRIBUTES
+		auto entity = ECS::CreateEntity();
+		auto &mesh = mMeshComponents.Create(entity);
+		mesh.mVertices = {
+			-0.95f, -0.95f, 0.0f,  // left
+			-0.0f,  -0.95f, 0.0f,  // right
+			-0.5f,   0.0f,  0.0f,  // top
+		};
+		mesh.mColours = {
+			0.0f, 1.0f, 0.0f,
+			0.0f, 0.0f, 1.0f,
+			1.0f, 0.0f, 0.0f};
+
+		mGraphicsContext->setHandle(mesh);
+	}
+	{ // TRIANGLE - COLOUR ATTRIBUTES
+		auto entity = ECS::CreateEntity();
+		auto &mesh = mMeshComponents.Create(entity);
+		mesh.mVertices = {
+			0.0f,  -0.95f, 0.0f, // left
+			0.95f, -0.95f, 0.0f, // right
+			0.5f,   0.0f,  0.0f	 // top
+		};
+		mesh.mColours = {
+			0.0f, 0.0f, 1.0f,
+			1.0f, 0.0f, 0.0f,
+			0.0f, 1.0f, 0.0f};
+		mGraphicsContext->setHandle(mesh);
+	}
+	{ // SQUARE - COLOUR ATTRIBUTES - ELEMENT BUFFER OBJECT DRAWN (mIndices)
+		auto entity = ECS::CreateEntity();
+		auto &mesh = mMeshComponents.Create(entity);
+		mesh.mVertices = {
+			-0.05f, 0.95f, 0.0f,
+			-0.05f, 0.0f,  0.0f,
+			-0.95f, 0.0f,  0.0f,
+			-0.95f, 0.95f, 0.0f,
+		};
+		mesh.mColours = {
+			0.0f, 0.0f, 1.0f,
+			0.0f, 1.0f, 0.0f,
+			1.0f, 0.0f, 0.0f,
+			1.0f, 1.0f, 0.0f};
+		mesh.mIndices = {
+			0, 1, 3, // first triangle
+			1, 2, 3	 // second triangle
+		};
+
+		mGraphicsContext->setHandle(mesh);
+	}
+	{ // SQUARE - TEXTURE
+		auto entity = ECS::CreateEntity();
+		auto &mesh = mMeshComponents.Create(entity);
+		mesh.mVertices = {
+			0.1f,  0.95f, 0.0f,
+			0.1f,  0.0f,  0.0f,
+			0.95f, 0.0f,  0.0f,
+			0.95f, 0.95f, 0.0f,
+		};
+		mesh.mColours = {
+			0.0f, 0.0f, 1.0f,
+			0.0f, 1.0f, 0.0f,
+			1.0f, 0.0f, 0.0f,
+			1.0f, 1.0f, 0.0f};
+		mesh.mTextureCoordinates = {
+			1.0f, 1.0f,
+			1.0f, 0.0f,
+			0.0f, 0.0f,
+			0.0f, 1.0f};
+		mesh.mTextures.push_back("woodenContainer.png");
+		mesh.mTextures.push_back("awesomeface.png");
+		mesh.mIndices = {
+			0, 1, 3, // first triangle
+			1, 2, 3	 // second triangle
+		};
+
+		mGraphicsContext->setHandle(mesh);
+	}
+
+	return true;
 }
 
 void Renderer::prepareFrame()
@@ -28,7 +107,6 @@ void Renderer::drawFrame()
 	// Draw all the render information components
 	for (size_t i = 0; i < mMeshComponents.GetCount(); ++i)
 	{
-		ZEPHYR_ASSERT(mMeshComponents[i].mDrawMode != Mesh::DrawMode::Unknown, "Attempting to draw a mesh that was un-initialised using the graphics context")
 		mGraphicsContext->draw(mMeshComponents[i]);
 	}
 
