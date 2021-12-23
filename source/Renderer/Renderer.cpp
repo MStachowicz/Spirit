3e4832a82755a6ed1f02c2ea4c1a331b566151bb
Michal S <michalstachowicz.95@gmail.com>
2021-12-23 21:36:46 +0000

    Added a 3D cube to the scene. Added scale options to the ImGui window.

--- source/Renderer/Renderer.cpp
+++ source/Renderer/Renderer.cpp
@@ -40,21 +40,23 @@ void Renderer::draw()
 	{
 		static float position[] = {0.0, 0.0, 0.0};
 		static float rotation[] = {0.0, 0.0, 0.0};
-		static float color[4] = {1.0f, 1.0f, 1.0f, 1.0f};
+		static float scale[] 	= {0.5, 0.5, 0.5};
+		static float color[4] 	= {1.0f, 1.0f, 1.0f, 1.0f};
 
 		if(ImGui::Begin("Container options"))
 		{
-			ImGui::SliderFloat3("Position", position, -1, 1);
-			ImGui::SliderFloat3("Rotation", rotation, -90, 90);
-			ImGui::ColorEdit3("color", color);
+			ImGui::SliderFloat3("Position"	, position, -1.f, 1.f);
+			ImGui::SliderFloat3("Rotation"	, rotation, -90.f, 90.f);
+			ImGui::SliderFloat3("Scale"		, scale, 0.1f, 1.5f);
+			ImGui::ColorEdit3("color"		, color);
 		}
 		ImGui::End();
 
 		DrawCall drawCall;
-		drawCall.mScale 			= glm::vec3(0.25f, 1.5f, 1.0f);
+		drawCall.mScale 			= glm::vec3(scale[0], scale[1], scale[2]);
 		drawCall.mPosition 			= glm::vec3(position[0], position[1], position[2]);
 		drawCall.mRotation 			= glm::vec3(rotation[0], rotation[1], rotation[2]);
-		drawCall.mMesh 				= mGraphicsContext->getMeshID("Square");
+		drawCall.mMesh 				= mGraphicsContext->getMeshID("Cube");
 		drawCall.mTexture 			= mGraphicsContext->getTextureID("woodenContainer.png");
 		mGraphicsContext->pushDrawCall(drawCall);
 	}
