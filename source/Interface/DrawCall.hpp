65e9dba11c0a8c9361f9ca2d563d75b9399090ac
Michal S <michalstachowicz.95@gmail.com>
2022-07-01 17:28:14 +0100

    Added the hash and equality operators for instancing DrawCalls.

--- source/Interface/DrawCall.hpp
+++ source/Interface/DrawCall.hpp
@@ -59,3 +59,46 @@ struct DrawCall
 
 	std::optional<float>	 mTextureRepeatFactor = std::nullopt;
 };
+
+struct DrawCallInstanceHash
+{
+	std::size_t operator()(const DrawCall &drawCall) const
+	{
+		std::size_t seed = 0;
+		util::HashCombine(seed, drawCall.mMesh);
+		util::HashCombine(seed, drawCall.mDrawMode);
+		util::HashCombine(seed, drawCall.mDrawStyle);
+
+		// Per DrawStyle values
+		util::HashCombine(seed, drawCall.mTexture1);
+		util::HashCombine(seed, drawCall.mTexture2);
+		util::HashCombine(seed, drawCall.mMixFactor);
+		util::HashCombine(seed, drawCall.mColour);
+		util::HashCombine(seed, drawCall.mDiffuseTextureID);
+		util::HashCombine(seed, drawCall.mSpecularTextureID);
+		util::HashCombine(seed, drawCall.mShininess);
+		util::HashCombine(seed, drawCall.mTextureRepeatFactor);
+
+		return seed;
+	}
+};
+
+struct DrawCallInstanceEqual
+{
+	bool operator()(const DrawCall &pDrawCall1, const DrawCall &pDrawCall2) const
+	{
+		return pDrawCall1.mMesh == pDrawCall2.mMesh
+		&& pDrawCall1.mDrawMode == pDrawCall2.mDrawMode
+		&& pDrawCall1.mDrawStyle == pDrawCall2.mDrawStyle
+
+		// Per DrawStyle values
+		&& pDrawCall1.mTexture1 == pDrawCall2.mTexture1
+		&& pDrawCall1.mTexture2 == pDrawCall2.mTexture2
+		&& pDrawCall1.mMixFactor == pDrawCall2.mMixFactor
+		&& pDrawCall1.mColour == pDrawCall2.mColour
+		&& pDrawCall1.mDiffuseTextureID == pDrawCall2.mDiffuseTextureID
+		&& pDrawCall1.mSpecularTextureID == pDrawCall2.mSpecularTextureID
+		&& pDrawCall1.mShininess == pDrawCall2.mShininess
+		&& pDrawCall1.mTextureRepeatFactor == pDrawCall2.mTextureRepeatFactor;
+	}
+};
