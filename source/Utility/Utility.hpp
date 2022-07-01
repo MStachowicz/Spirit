a5dbbd6ab65a78af2dbc22b81aae91acb06e3c83
Michal S <michalstachowicz.95@gmail.com>
2022-07-01 16:11:32 +0100

    Added Model matrix construction helper function.

--- source/Utility/Utility.hpp
+++ source/Utility/Utility.hpp
@@ -61,6 +61,16 @@ namespace util
         pSeed ^= hasher(pValue) + 0x9e3779b9 + (pSeed << 6) + (pSeed >> 2);
     }
 
+    inline glm::mat4 GetModelMatrix(const glm::vec3& pPosition, const glm::vec3& pRotation, const glm::vec3& pScale)
+    {
+        glm::mat4 model = glm::translate(glm::identity<glm::mat4>(), pPosition);
+        model = glm::rotate(model, glm::radians(pRotation.x), glm::vec3(1.0, 0.0, 0.0));
+        model = glm::rotate(model, glm::radians(pRotation.y), glm::vec3(0.0, 1.0, 0.0));
+        model = glm::rotate(model, glm::radians(pRotation.z), glm::vec3(0.0, 0.0, 1.0));
+        model = glm::scale(model, pScale);
+        return model;
+    }
+
     class File
     {
     public:
