3e505ecfa5ea43ca75c3da34acb89f7940ed9e32
Michal S <michalstachowicz.95@gmail.com>
2022-11-26 17:26:28 +0000

    Added variable symbol unit information comments.

--- source/Utility/Geometry.hpp
+++ source/Utility/Geometry.hpp
@@ -2,9 +2,22 @@
 
 #include "glm/vec3.hpp"
 
+// Variable          Symbol     SI Unit
+// -------------------------------------
+//Force                F          (N)
+//Momentum             p        (kg m/s)
+//Acceleration         a        (m/s²)
+//Velocity             v         (m/s)
+//Mass                 m          (kg)
+//Torque               T         (N m)
+//Angular Momentum     L       (kg m²/s)
+//Angular Velocity     ω        (rad/s)
+//Inertia              I        (kg m²)
+
+// All the Geometry functions expect all params and return all values in SI units.
 namespace Geometry
 {
-    // Get the moment of inertia (I) for a solid cuboid in SI units (kg m²)
+    // Get the moment of inertia (I) for a solid cuboid in SI units (kg m²) about central axis along height.
     static glm::vec3 CuboidInertia(float pMass, float pHeight, float pWidth, float pDepth)
     {
         const float InertiaX = (1.f/12.f) * pMass * (std::pow(pDepth, 2.f) + std::pow(pHeight, 2.f));
