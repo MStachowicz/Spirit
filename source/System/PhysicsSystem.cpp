0f271c198f0e53c3450a237a95f0bff520bd5ac5
Michal S <michalstachowicz.95@gmail.com>
2022-11-26 23:24:28 +0000

    Added angular motion using torque. Closes #25

--- source/System/PhysicsSystem.cpp
+++ source/System/PhysicsSystem.cpp
@@ -4,6 +4,8 @@
 #include "RigidBody.hpp"
 #include "Transform.hpp"
 
+#include "Utility.hpp"
+
 namespace System
 {
     PhysicsSystem::PhysicsSystem(ECS::Storage& pStorage)
@@ -24,16 +26,44 @@ namespace System
             if (pRigidBody.mApplyGravity)
                 pRigidBody.mForce = pRigidBody.mMass * mGravity;
 
-            // Change in momentum is equal to the force = dp/dt = F
-            const auto changeInMomentum = pRigidBody.mForce * pDeltaTime.count(); // dp = F dt
-            pRigidBody.mMomentum += changeInMomentum;
+            { // Linear motion
+                // Change in momentum is equal to the force = dp/dt = F
+                const auto changeInMomentum = pRigidBody.mForce * pDeltaTime.count(); // dp = F dt
+                pRigidBody.mMomentum += changeInMomentum;
 
-            // Convert momentum to velocity by dividing by mass: p = mv
-            pRigidBody.mVelocity = pRigidBody.mMomentum / pRigidBody.mMass; // v = p/v
+                // Convert momentum to velocity by dividing by mass: p = mv
+                pRigidBody.mVelocity = pRigidBody.mMomentum / pRigidBody.mMass; // v = p/v
 
-            // Integrate velocity to find new position: dx/dt = v
-            const auto changeInPosition = pRigidBody.mVelocity * pDeltaTime.count(); // dx = v dt
-            pTransform.mPosition += changeInPosition;
+                // Integrate velocity to find new position: dx/dt = v
+                const auto changeInPosition = pRigidBody.mVelocity * pDeltaTime.count(); // dx = v dt
+                pTransform.mPosition += changeInPosition;
+            }
+
+            { // Angular motion
+                // http://physics.bu.edu/~redner/211-sp06/class-rigid-body/angularmo.html
+                const auto changeInAngularMomentum = pRigidBody.mTorque * pDeltaTime.count(); // dL = T dt
+                pRigidBody.mAngularMomentum += changeInAngularMomentum;
+
+                // Convert angular momentum to angular velocity by dividing by inertia tensor: L = Iω
+                pRigidBody.mAngularVelocity = pRigidBody.mAngularMomentum / pRigidBody.mInertia; // ω = L / I
+                //pRigidBody.mAngularVelocity = glm::vec3(0.f, glm::radians(360.f), 0.f) * pDeltaTime.count();
+
+                // To integrate the new quat orientation we convert the angular velocity into quaternion form - spin.
+                // Spin represents a time derivative of orientation. https://www.cs.cmu.edu/~baraff/sigcourse/notesd1.pdf
+                const glm::quat spin =
+                    0.5f * glm::quat(0.f, pRigidBody.mAngularVelocity.x, pRigidBody.mAngularVelocity.y, pRigidBody.mAngularVelocity.z) * pTransform.mOrientation;
+
+                // Integrate spin to find the new orientation
+                pTransform.mOrientation += spin;
+                pTransform.mOrientation = glm::normalize(pTransform.mOrientation);
+                // Recalculate the direction and rotation mat
+                pTransform.mDirection = glm::normalize(pTransform.mOrientation * Component::Transform::StartingDirection);
+                pTransform.mRollPitchYaw = glm::degrees(Utility::toRollPitchYaw(pTransform.mOrientation));
+            }
+
+            pTransform.mModel = glm::translate(glm::identity<glm::mat4>(), pTransform.mPosition);
+            pTransform.mModel *= glm::mat4_cast(pTransform.mOrientation);
+            pTransform.mModel = glm::scale(pTransform.mModel, pTransform.mScale);
         });
     }
 } // namespace System
