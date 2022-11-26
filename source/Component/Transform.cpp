021049edb5f605dd9a8f0c1512a5885ff5575af9
Michal S <michalstachowicz.95@gmail.com>
2022-11-26 09:54:16 +0000

    Added Orientation and direction to Transform. #25 Added Rotation editor using Euler angles converting to quat.

--- source/Component/Transform.cpp
+++ source/Component/Transform.cpp
@@ -1,17 +1,31 @@
 #include "Transform.hpp"
 
 #include "imgui.h"
+#include "glm/gtx/euler_angles.hpp"
+
+#include <string>
 
 namespace Component
 {
-  void Transform::DrawImGui()
-  {
-    if(ImGui::TreeNode("Transform"))
+    void Transform::DrawImGui()
     {
-      ImGui::SliderFloat3("Position", &mPosition.x, -50.f, 50.f);
-      ImGui::SliderFloat3("Rotation", &mRotation.x, -360.f, 360.f);
-      ImGui::SliderFloat3("Scale", &mScale.x, 0.1f, 10.f);
-      ImGui::TreePop();
+        if (ImGui::TreeNode("Transform"))
+        {
+            ImGui::SliderFloat3("Position (m)", &mPosition.x, -50.f, 50.f);
+            ImGui::SliderFloat3("Scale", &mScale.x, 0.1f, 10.f);
+
+            if (ImGui::SliderFloat3("Rotation (degrees)", &mRotation.x, -360.f, 360.f))
+            {
+                const auto rotationMat = glm::inverse(glm::eulerAngleXYZ(glm::radians(mRotation.x), glm::radians(mRotation.y), glm::radians(mRotation.z)));
+                mDirection = glm::vec3(glm::vec4(0.f, 0.f, 1.f, 0.f) * rotationMat);
+                mOrientation = glm::normalize(glm::toQuat(rotationMat));
+            }
+
+            ImGui::Separator();
+            ImGui::Text(("Directon:    " + std::to_string(mDirection.x) + ", " + std::to_string(mDirection.y) + ", " + std::to_string(mDirection.z)).c_str());
+            ImGui::Text(("Orientation: " + std::to_string(mOrientation.w) + ", " + std::to_string(mOrientation.x) + ", " + std::to_string(mOrientation.y) + ", " + std::to_string(mOrientation.z)).c_str());
+
+            ImGui::TreePop();
+        }
     }
-  }
-}
+} // namespace Component
--- source/Component/Transform.hpp
+++ source/Component/Transform.hpp
@@ -1,22 +1,18 @@
 #pragma once
 
 #include "glm/vec3.hpp"
+#include "glm/gtx/quaternion.hpp"
 
 namespace Component
 {
     struct Transform
     {
-        glm::vec3 mPosition = glm::vec3(0.0f);
-        glm::vec3 mRotation = glm::vec3(0.0f);
-        glm::vec3 mScale    = glm::vec3(1.0f);
+        glm::vec3 mPosition    = glm::vec3(0.0f); // World space position.
+        glm::vec3 mRotation    = glm::vec3(0.0f); // Rotation represented in Euler angles. Range [-360 - 360].
+        glm::vec3 mScale       = glm::vec3(1.0f);
+        glm::vec3 mDirection   = glm::vec3(0.f, 0.f, 1.f); // Unit vector giving the direction the entity is facing.
+        glm::quat mOrientation = glm::identity<glm::quat>(); // Unit quaternion taking the forward direction (0,0,1) to the current orientation.
 
         void DrawImGui();
-        bool operator==(const Transform& pOther) const
-        {
-            return mPosition == pOther.mPosition &&
-                mRotation == pOther.mRotation &&
-                mScale == pOther.mScale;
-        }
-        bool operator != (const Transform& pOther) const { return !(*this == pOther); }
     };
 }
