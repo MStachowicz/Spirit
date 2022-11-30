a13c54199a130b6594a2e0e0ad4457f8fcf79c79
Michal S <michalstachowicz.95@gmail.com>
2022-11-30 18:08:42 +0000

    Added constructor.

--- source/Geometry/Cylinder.hpp
+++ source/Geometry/Cylinder.hpp
@@ -6,6 +6,13 @@ namespace Geometry
 {
     class Cylinder
     {
+    public:
+        Cylinder(const glm::vec3& pBase, const glm::vec3& pTop, const float& pDiameter)
+        : mBase{pBase}
+        , mTop{pTop}
+        , mDiameter{pDiameter}
+        {}
+
         glm::vec3 mBase;
         glm::vec3 mTop;
         float mDiameter;
