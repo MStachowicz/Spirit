6dda30db62feb5fd643c774d7c330b11136676b0
Michal S <michalstachowicz.95@gmail.com>
2023-01-03 00:14:30 +0000

    Added inertia tensor calculators Added cylinder and cuboid calculators

--- source/Geometry/Geometry.hpp
+++ source/Geometry/Geometry.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "glm/vec3.hpp"
+#include "glm/glm.hpp"
+
+#include <cmath>
 
 // Variable          Symbol     SI Unit
 // -------------------------------------
@@ -27,7 +30,28 @@ namespace Geometry
         return glm::vec3(InertiaX, InertiaY, InertiaZ);
     }
 
-    // Returns the magnitude of the impulse after a collision between two bodies 1 and 2.
+    static glm::mat3 cylinderInertiaTensor(const float& pMass, const float& pRadius, const float& pHeight)
+    {
+        // https://en.wikipedia.org/wiki/List_of_moments_of_inertia
+        // Cylinder oriented with height along z-axis
+        const float x = (1.f / 12.f) * pMass * ((3.f * std::pow(pRadius, 2.f)) + std::pow(pHeight, 2.f));
+        const float y = ((1.f / 2.f) * pMass * std::pow(pRadius, 2.f));
+
+        return glm::mat3(  x , 0.f, 0.f
+                        , 0.f,  x , 0.f
+                        , 0.f, 0.f,  y );
+    }
+    static glm::mat3 cuboidInertiaTensor(const float& pMass, const float& pWidth, const float& pHeight, const float& pDepth)
+    {
+        // https://en.wikipedia.org/wiki/List_of_moments_of_inertia
+
+        const float x = (1.f / 12.f) * pMass;
+        return glm::mat3( x * (std::pow(pHeight, 2.f) + std::pow(pDepth, 2.f)), 0.f, 0.f
+                        , 0.f, x * (std::pow(pWidth, 2.f) + std::pow(pHeight, 2.f)), 0.f
+                        , 0.f, 0.f,  x * (std::pow(pWidth, 2.f) + std::pow(pDepth, 2.f)) );
+    }
+
+    // Returns the magnitude of the impulse after a collision between bodies 1 and 2.
     // Impulse is a vector quantity, which can be derived by multiplying this magnitude with pCollisionNormal for each body.
     // pRestitution ranges from 0 to 1 where 1 is a perfectly elastic collision and 0 is perfectly inelastic.
     static float linearImpulseMagnitude(const float& pMass1, const glm::vec3& pVelocity1, const float& pMass2, const glm::vec3& pVelocity2, const glm::vec3& pCollisionNormal, const float& pRestitution)
