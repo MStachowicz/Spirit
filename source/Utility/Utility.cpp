017bd4ae246a55c85b1cd8466d58cd5aa9284306
Michal S <michalstachowicz.95@gmail.com>
2023-04-18 19:11:24 +0100

    Missing glm namespace usage.

--- source/Utility/Utility.cpp
+++ source/Utility/Utility.cpp
@@ -64,10 +64,10 @@ namespace Utility
     {
         // Reference: https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
 
-        const glm::vec3 start       = normalize(pStart);
-        const glm::vec3 destination = normalize(pDestination);
+        const glm::vec3 start       = glm::normalize(pStart);
+        const glm::vec3 destination = glm::normalize(pDestination);
 
-        const float cosTheta = dot(start, destination);
+        const float cosTheta = glm::dot(start, destination);
         glm::vec3 rotationAxis;
 
         if (cosTheta < -1.f + 0.001f)
