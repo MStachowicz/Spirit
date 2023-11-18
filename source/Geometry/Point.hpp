50b3442de1c4205f84b31cd6250ce94ca44918f4
MichalSt <michalstachowicz.95@gmail.com>
2023-11-18 17:58:57 +0000

    Add conversion operators to point.

--- source/Geometry/Point.hpp
+++ source/Geometry/Point.hpp
@@ -8,6 +8,8 @@ namespace Geometry
 	{
 	public:
 		constexpr Point(const glm::vec3& p_position) noexcept : m_position{p_position} {}
+		constexpr operator glm::vec3&() noexcept             { return m_position; }
+		constexpr operator const glm::vec3&() const noexcept { return m_position; }
 
 		glm::vec3 m_position;
 	};
