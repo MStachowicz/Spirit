43000c826daa0ae509e57c0e8eed97efaaff31c4
MichalSt <michalstachowicz.95@gmail.com>
2023-11-18 18:25:23 +0000

    Add length, direction, and point_along_ray methods.

--- source/Geometry/LineSegment.hpp
+++ source/Geometry/LineSegment.hpp
@@ -4,16 +4,28 @@
 
 namespace Geometry
 {
-    // A directed line segment. A finite section of a Line from m_start to m_end.
-    class LineSegment
-    {
-    public:
-        constexpr LineSegment(const glm::vec3& p_start, const glm::vec3& p_end) noexcept
-            : m_start{p_start}
-            , m_end{p_end}
-        {}
+	// A directed line segment. A finite section of a Line from m_start to m_end.
+	class LineSegment
+	{
+	public:
+		constexpr LineSegment(const glm::vec3& p_start, const glm::vec3& p_end) noexcept
+			: m_start{p_start}
+			, m_end{p_end}
+		{}
+		constexpr float length() const noexcept
+		{
+			return glm::length(m_end - m_start);
+		}
+		constexpr glm::vec3 direction() const noexcept
+		{
+			return glm::normalize(m_end - m_start);
+		}
+		constexpr glm::vec3 pointAt(const float& p_distance_along_ray) const noexcept
+		{
+			return m_start + (direction() * p_distance_along_ray);
+		}
 
-        glm::vec3 m_start;
-        glm::vec3 m_end;
-    };
+		glm::vec3 m_start;
+		glm::vec3 m_end;
+	};
 }
