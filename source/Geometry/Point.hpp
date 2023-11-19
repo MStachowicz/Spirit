f07e24eb856601c39195b80248c354e3287d8c72
MichalSt <michalstachowicz.95@gmail.com>
2023-11-19 16:51:36 +0000

    Add member functions for equality and inequality.

--- source/Geometry/Point.hpp
+++ source/Geometry/Point.hpp
@@ -7,10 +7,12 @@ namespace Geometry
 	class Point
 	{
 	public:
+		glm::vec3 m_position;
+
 		constexpr Point(const glm::vec3& p_position) noexcept : m_position{p_position} {}
 		constexpr operator glm::vec3&() noexcept             { return m_position; }
 		constexpr operator const glm::vec3&() const noexcept { return m_position; }
-
-		glm::vec3 m_position;
+		constexpr bool operator==(const Point& p_other) const noexcept { return m_position == p_other.m_position; }
+		constexpr bool operator!=(const Point& p_other) const noexcept { return m_position != p_other.m_position; }
 	};
 }
