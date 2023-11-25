b718aa69692959a756e8f1699da2b860435698ac
MichalSt <michalstachowicz.95@gmail.com>
2023-11-25 18:53:08 +0000

    Add test grouping via SCOPE_SECTION macro. Added std formatting for Geometry::Triangle. Moved UnitTest, PerformanceTest and new ScopeSection structs inside TestManager.

--- source/Test/TestManager.cpp
+++ source/Test/TestManager.cpp
@@ -10,98 +10,114 @@
 
 namespace Test
 {
-    static const std::string seperator = "*****************************************************************\n";
+	static const std::string seperator = "*****************************************************************\n";
 
-    std::string to_string(const std::source_location& p_location)
-    {
-        return std::format("{}:{}", p_location.file_name(), p_location.line());
-    }
+	std::string to_string(const std::source_location& p_location)
+	{
+		return std::format("{}:{}", p_location.file_name(), p_location.line());
+	}
 
-    void runUnitTests(const bool& pRunPerformanceTests)
-    {
-        LOG("{} Starting Unit tests", seperator);
-        Utility::Stopwatch stopwatch;
+	void runUnitTests(const bool& pRunPerformanceTests)
+	{
+		LOG("{} Starting Unit tests", seperator);
+		Utility::Stopwatch stopwatch;
 
-        ECSTester tester;
-        tester.run(pRunPerformanceTests);
+		ECSTester tester;
+		tester.run(pRunPerformanceTests);
 
-        GeometryTester geometryTester;
-        geometryTester.run(pRunPerformanceTests);
+		GeometryTester geometryTester;
+		geometryTester.run(pRunPerformanceTests);
 
-        ResourceManagerTester resource_manager_tester;
-        resource_manager_tester.run(pRunPerformanceTests);
+		ResourceManagerTester resource_manager_tester;
+		resource_manager_tester.run(pRunPerformanceTests);
 
-        LOG("All Unit tests complete - Time taken: {}ms\n{}", stopwatch.duration_since_start<float, std::milli>().count(), seperator);
-    }
+		LOG("All Unit tests complete - Time taken: {}ms\n{}", stopwatch.duration_since_start<float, std::milli>().count(), seperator);
+	}
 
-    UnitTest::UnitTest(const bool& pCondition, const std::string& pName, const std::string& pFailMessage) noexcept
-        : mPassed{pCondition}
-        , mName{pName}
-        , mFailMessage{pFailMessage}
-    {}
-    TestManager::TestManager(const std::string& pName) noexcept
-        : mName{pName}
-        , mUnitTestsPassed{0}
-        , mUnitTestsFailed{0}
-        , mUnitTests{}
-        , mTimeTakenUnitTests{}
-        , mPerformanceTests{}
-        , mTimeTakenPerformanceTests{}
-    {}
+	TestManager::UnitTest::UnitTest(const bool& pCondition, const std::string& pName, const std::string& pFailMessage) noexcept
+		: mPassed{pCondition}
+		, mName{pName}
+		, mFailMessage{pFailMessage}
+	{}
+	TestManager::TestManager(const std::string& pName) noexcept
+		: mName{pName}
+		, section_name_lengths{}
+		, running_section_name{""}
+		, mUnitTestsPassed{0}
+		, mUnitTestsFailed{0}
+		, mUnitTests{}
+		, mTimeTakenUnitTests{}
+		, mPerformanceTests{}
+		, mTimeTakenPerformanceTests{}
+	{}
 
-    void TestManager::runUnitTest(const UnitTest&& pTest)
-    {
-        mUnitTests.emplace_back(std::forward<const UnitTest>(pTest));
-        if (mUnitTests.back().mPassed)
-            mUnitTestsPassed++;
-        else
-            mUnitTestsFailed++;
-    }
-    void TestManager::runPerformanceTest(const PerformanceTest&& pTest)
-    {
-        mPerformanceTests.emplace_back(std::forward<const PerformanceTest>(pTest));
-    }
+	void TestManager::runUnitTest(UnitTest&& pTest)
+	{
+		if (!running_section_name.empty())
+			pTest.mName = running_section_name + " - " + pTest.mName;
 
-    void TestManager::run(const bool& pRunPerformanceTests)
-    {
-        { // Unit Tests
-            Utility::Stopwatch stopwatch;
-            runUnitTests();
-            mTimeTakenUnitTests = stopwatch.duration_since_start<float, std::milli>();
-        }
-        if (pRunPerformanceTests)
-        {
-            Utility::Stopwatch stopwatch;
-            runPerformanceTests();
-            mTimeTakenPerformanceTests = stopwatch.duration_since_start<float, std::milli>();
-        }
+		mUnitTests.emplace_back(std::forward<const UnitTest>(pTest));
 
-        std::string output = seperator + std::format("\n----------------- {} TESTS STARTING -----------------\n", mName);
-        output.reserve((mUnitTests.size() + mPerformanceTests.size()) * (60)); // Assuming an average of 60 chars per test summary here
-        { // Build the output string
-            for (const auto& test : mUnitTests)
-            {
-                if (test.mPassed)
-                    output += std::format("PASSED '{}'\n", test.mName);
-                else
-                    output += std::format("FAILED '{}' -> {}\n", test.mName, test.mFailMessage);
-            }
-            for (const auto& test : mPerformanceTests)
-                output += std::format("PERF TEST '{}' - TOOK {}ms\n", test.mName, test.mTimeTaken.count());
+		if (mUnitTests.back().mPassed)
+			mUnitTestsPassed++;
+		else
+			mUnitTestsFailed++;
+	}
+	void TestManager::push_section(const std::string& p_section_name)
+	{
+		running_section_name += '[' + p_section_name + ']';
+		section_name_lengths.push_back(p_section_name.length() + 2); // +2 for the [] around the section name
+	}
+	void TestManager::pop_section()
+	{
+		running_section_name = running_section_name.substr(0, running_section_name.length() - section_name_lengths.back());
+		section_name_lengths.pop_back();
+	}
+	void TestManager::runPerformanceTest(const PerformanceTest&& pTest)
+	{
+		mPerformanceTests.emplace_back(std::forward<const PerformanceTest>(pTest));
+	}
 
-            output += std::format("***************** {} TEST SUMMARY *****************\n", mName);
-            output += std::format("----------------- UNIT TESTS -----------------\n");
-            output += std::format("TOTAL TESTS: {}\nPASSED: {}\nFAILED: {}\nTIME TAKEN: {}ms\n", mUnitTests.size(), mUnitTestsPassed, mUnitTestsFailed, mTimeTakenUnitTests.count());
+	void TestManager::run(const bool& pRunPerformanceTests)
+	{
+		{ // Unit Tests
+			Utility::Stopwatch stopwatch;
+			runUnitTests();
+			mTimeTakenUnitTests = stopwatch.duration_since_start<float, std::milli>();
+		}
+		if (pRunPerformanceTests)
+		{
+			Utility::Stopwatch stopwatch;
+			runPerformanceTests();
+			mTimeTakenPerformanceTests = stopwatch.duration_since_start<float, std::milli>();
+		}
 
-            if (pRunPerformanceTests)
-            {
-                output += std::format("----------------- PERFORMANCE TESTS -----------------\n");
-                output += std::format("TOTAL TESTS: {}\nTIME TAKEN: {}ms\n", mPerformanceTests.size(), mTimeTakenPerformanceTests.count());
-            }
-            output += seperator;
-        }
+		std::string output = seperator + std::format("\n----------------- {} TESTS STARTING -----------------\n", mName);
+		output.reserve((mUnitTests.size() + mPerformanceTests.size()) * (60)); // Assuming an average of 60 chars per test summary here
+		{ // Build the output string
+			for (const auto& test : mUnitTests)
+			{
+				if (test.mPassed)
+					output += std::format("PASSED '{}'\n", test.mName);
+				else
+					output += std::format("FAILED '{}' -> {}\n", test.mName, test.mFailMessage);
+			}
+			for (const auto& test : mPerformanceTests)
+				output += std::format("PERF TEST '{}' - TOOK {}ms\n", test.mName, test.mTimeTaken.count());
 
-        LOG("{}", output);
-    }
+			output += std::format("***************** {} TEST SUMMARY *****************\n", mName);
+			output += std::format("----------------- UNIT TESTS -----------------\n");
+			output += std::format("TOTAL TESTS: {}\nPASSED: {}\nFAILED: {}\nTIME TAKEN: {}ms\n", mUnitTests.size(), mUnitTestsPassed, mUnitTestsFailed, mTimeTakenUnitTests.count());
+
+			if (pRunPerformanceTests)
+			{
+				output += std::format("----------------- PERFORMANCE TESTS -----------------\n");
+				output += std::format("TOTAL TESTS: {}\nTIME TAKEN: {}ms\n", mPerformanceTests.size(), mTimeTakenPerformanceTests.count());
+			}
+			output += seperator;
+		}
+
+		LOG("{}", output);
+	}
 
 } // namespace Test
--- source/Test/TestManager.hpp
+++ source/Test/TestManager.hpp
@@ -4,6 +4,7 @@
 
 #include "glm/glm.hpp"
 #include "Geometry/Point.hpp"
+#include "Geometry/Triangle.hpp"
 
 #include <string>
 #include <vector>
@@ -13,102 +14,125 @@
 
 namespace std
 {
-    template<>
-    struct formatter<glm::vec3>
+	template<>
+	struct formatter<glm::vec3>
 	{
-        constexpr auto parse(format_parse_context& ctx) { return ctx.begin(); }
-
-        auto format(const glm::vec3& v, format_context& ctx) const { return format_to(ctx.out(), "({}, {}, {})", v.x, v.y, v.z); }
-    };
-    template<>
-    struct formatter<Geometry::Point>
+		constexpr auto parse(format_parse_context& ctx) { return ctx.begin(); }
+		auto format(const glm::vec3& v, format_context& ctx) const { return format_to(ctx.out(), "({}, {}, {})", v.x, v.y, v.z); }
+	};
+	template<>
+	struct formatter<Geometry::Point>
 	{
-        constexpr auto parse(format_parse_context& ctx) { return ctx.begin(); }
-
-        auto format(const Geometry::Point& v, format_context& ctx) const { return format_to(ctx.out(), "({}, {}, {})", v.m_position.x, v.m_position.y, v.m_position.z); }
-    };
-    template<>
-    struct formatter<std::optional<Geometry::Point>>
+		constexpr auto parse(format_parse_context& ctx) { return ctx.begin(); }
+		auto format(const Geometry::Point& v, format_context& ctx) const { return format_to(ctx.out(), "({}, {}, {})", v.m_position.x, v.m_position.y, v.m_position.z); }
+	};
+	template<>
+	struct formatter<std::optional<Geometry::Point>>
 	{
-        constexpr auto parse(format_parse_context& ctx) { return ctx.begin(); }
-
-        auto format(const std::optional<Geometry::Point>& v, format_context& ctx) const
-		{
-			return v.has_value() ? format_to(ctx.out(), "({}, {}, {})", v->m_position.x, v->m_position.y, v->m_position.z) : format_to(ctx.out(), "nullopt");
-		}
+		constexpr auto parse(format_parse_context& ctx) { return ctx.begin(); }
+		auto format(const std::optional<Geometry::Point>& v, format_context& ctx) const { return v.has_value() ? format_to(ctx.out(), "({}, {}, {})", v->m_position.x, v->m_position.y, v->m_position.z) : format_to(ctx.out(), "nullopt"); }
+	};
+	template<>
+	struct formatter<Geometry::Triangle>
+	{
+		constexpr auto parse(format_parse_context& ctx) { return ctx.begin(); }
+		auto format(const Geometry::Triangle& v, format_context& ctx) { return format_to(ctx.out(), "({}, {}, {})", v.m_point_1, v.m_point_2, v.m_point_3); }
 	};
 }
 
 namespace Test
 {
-    // Modifies source_location into a IDE hyperlink friendly format
-    std::string to_string(const std::source_location& p_location);
+	// Modifies source_location into a IDE hyperlink friendly format
+	std::string to_string(const std::source_location& p_location);
 
-    #define CHECK_TRUE(p_conditional, p_test_name)        { runUnitTest({p_conditional, p_test_name,         std::format("Expected: '{}' to be true\n{}", #p_conditional,        to_string(std::source_location::current()))}); }
-    #define CHECK_EQUAL(p_value, p_expected_value, p_test_name) { runUnitTest({p_value == p_expected_value, p_test_name, std::format("Expected: '{}' to be '{}' but was '{}'\n{}", #p_value, #p_expected_value, p_value, to_string(std::source_location::current()))}); }
-    //TODO: ADD CHECK_THROW (make sure the conditional or func throws and exception)
+	#define CHECK_TRUE(p_conditional, p_test_name) { runUnitTest({p_conditional, p_test_name,         std::format("Expected: '{}' to be true\n{}", #p_conditional,        to_string(std::source_location::current()))}); }
+	#define CHECK_EQUAL(p_value, p_expected_value, p_test_name) { runUnitTest({p_value == p_expected_value, p_test_name, std::format("Expected: '{}' to be '{}' but was '{}'\n{}", #p_value, #p_expected_value, p_value, to_string(std::source_location::current()))}); }
+	#define SCOPE_SECTION(p_section_name) auto a_random_name_that_will_never_collide_with_anything = ScopeSection(p_section_name, *this);
 
-    using TestDuration = std::chrono::duration<float, std::milli>;
+	using TestDuration = std::chrono::duration<float, std::milli>;
 
-    void runUnitTests(const bool& pRunPerformanceTests);
+	void runUnitTests(const bool& pRunPerformanceTests);
 
-    // Represents a single unit test.
-    class UnitTest
-    {
-    public:
-        UnitTest(const bool& pCondition, const std::string& pName, const std::string& pFailMessage) noexcept;
-        bool mPassed;       // Did the test pass
-        std::string mName;  // Title of the test
-        std::string mFailMessage; // Message outputted if the test fails
-    };
-    // Represents the mTimeTaken to execute the Func passed as the constructor parameter pTestFunc.
-    class PerformanceTest
-    {
-        static constexpr size_t RepeatCount = 10; // Number of times the test will be repeated to find a more accurate average time taken.
-    public:
-        std::string mName;  // Title of the test
-        TestDuration mTimeTaken;
+	// A pure-virtual API for running unit tests and performance tests.
+	// Override the runUnitTests and runPerformanceTests functions to add your own tests.
+	// When run() is called, the results of all the tests will be outputted.
+	class TestManager
+	{
+	protected:
+		// Represents a section of tests pushing and popping the p_section_name to the TestManager running_section_name
+		// on construction and destruction respectively via push_section and pop_section.
+		struct ScopeSection
+		{
+			TestManager& test_manager;
 
-        template<typename Func>
-        PerformanceTest(const std::string& pName, Func& pTestFunc) noexcept
-            : mName{pName}
-            , mTimeTaken{}
-        {
-            Utility::Stopwatch stopwatch;
-            for (size_t i = 0; i < RepeatCount; i++)
-                pTestFunc();
-            mTimeTaken = stopwatch.duration_since_start<float, std::milli>() / RepeatCount; // Divide to find the average time taken.
-        }
-    };
-    // A pure-virtual API for running unit tests and performance tests.
-    // Override the runUnitTests and runPerformanceTests functions to add your own tests.
-    // When run() is called, the results of all the tests will be outputted.
-    class TestManager
-    {
-        std::string mName;
+			ScopeSection(const std::string& p_section_name, TestManager& test_manager) noexcept
+				: test_manager{test_manager}
+			{
+				test_manager.push_section(p_section_name);
+			}
+			~ScopeSection() noexcept
+			{
+				test_manager.pop_section();
+			}
+		};
+		friend ScopeSection;
+		// Represents a single unit test. Pushed to the TestManager using runUnitTest via CHECK_TRUE and CHECK_EQUAL macros.
+		struct UnitTest
+		{
+			UnitTest(const bool& pCondition, const std::string& pName, const std::string& pFailMessage) noexcept;
+			bool mPassed;       // Did the test pass
+			std::string mName;  // Title of the test
+			std::string mFailMessage; // Message outputted if the test fails
+		};
+		// Represents the mTimeTaken to execute the Func passed as the constructor parameter pTestFunc.
+		struct PerformanceTest
+		{
+			static constexpr size_t RepeatCount = 10; // Number of times the test will be repeated to find a more accurate average time taken.
 
-        size_t mUnitTestsPassed;
-        size_t mUnitTestsFailed;
-        std::vector<UnitTest> mUnitTests;
-        TestDuration mTimeTakenUnitTests; // Time taken to complete all the unit tests
+			std::string mName;  // Title of the test
+			TestDuration mTimeTaken;
 
-        std::vector<PerformanceTest> mPerformanceTests;
-        TestDuration mTimeTakenPerformanceTests; // Time taken to complete all the performance tests
+			template<typename Func>
+			PerformanceTest(const std::string& pName, Func& pTestFunc) noexcept
+				: mName{pName}
+				, mTimeTaken{}
+			{
+				Utility::Stopwatch stopwatch;
+				for (size_t i = 0; i < RepeatCount; i++)
+					pTestFunc();
+				mTimeTaken = stopwatch.duration_since_start<float, std::milli>() / RepeatCount; // Divide to find the average time taken.
+			}
+		};
 
-    protected:
-        // Pushes the test to mUnitTests and updates the running totals.
-        void runUnitTest(const UnitTest&& pTest);
-        // Pushes the test to mPerformance tests and updates the running totals.
-        void runPerformanceTest(const PerformanceTest&& pTest);
 
-        virtual void runUnitTests()        = 0;
-        virtual void runPerformanceTests() = 0;
+		// Pushes the test to mUnitTests and updates the running totals.
+		void runUnitTest(UnitTest&& pTest);
+		// Pushes the test to mPerformance tests and updates the running totals.
+		void runPerformanceTest(const PerformanceTest&& pTest);
 
-    public:
-        TestManager(const std::string& pName) noexcept;
+		void push_section(const std::string& p_section_name);
+		void pop_section();
+		virtual void runUnitTests()        = 0;
+		virtual void runPerformanceTests() = 0;
 
-        // Executes all the tests filling up the mUnitTests and mPerformanceTests containers.
-        // When completed, outputs the results of all the tests.
-        void run(const bool& pRunPerformanceTests);
-    };
+	public:
+		TestManager(const std::string& pName) noexcept;
+
+		// Executes all the tests filling up the mUnitTests and mPerformanceTests containers.
+		// When completed, outputs the results of all the tests.
+		void run(const bool& pRunPerformanceTests);
+
+	private:
+		std::string mName;
+		std::vector<size_t> section_name_lengths;
+		std::string running_section_name;
+
+		size_t mUnitTestsPassed;
+		size_t mUnitTestsFailed;
+		std::vector<UnitTest> mUnitTests;
+		TestDuration mTimeTakenUnitTests; // Time taken to complete all the unit tests
+
+		std::vector<PerformanceTest> mPerformanceTests;
+		TestDuration mTimeTakenPerformanceTests; // Time taken to complete all the performance tests
+	};
 } // namespace Test
