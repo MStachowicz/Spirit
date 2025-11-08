87d6c747c28aaebbbe025b1850f5e78cefb2eec1
Michal Stachowicz <michalstachowicz.95@gmail.com>
2025-11-08 19:37:10 +0000

    Added a Jolt 'hello world'. Basic copy of the hello world Jolt supplies to test the simulation running and linking into the engine.

--- source/System/PhysicsSystem.hpp
+++ source/System/PhysicsSystem.hpp
@@ -13,20 +13,21 @@ namespace System
 	// The system is force based and numerically integrates
 	class PhysicsSystem
 	{
-	public:
-		PhysicsSystem(SceneSystem& scene_system, CollisionSystem& collision_system);
-		void integrate(const DeltaTime& delta_time);
-
-		size_t m_update_count;
-		float m_restitution;             // Coefficient of restitution applied in collision response.
-		bool m_apply_collision_response; // Whether to apply collision response or not.
-		bool m_bool_apply_kinematic;     // Whether to apply kinematic equations or not.
-
-	private:
 		SceneSystem& m_scene_system;
 		CollisionSystem& m_collision_system;
 
 		DeltaTime m_total_simulation_time; // Total time simulated using the integrate function.
 		glm::vec3 m_gravity;               // The acceleration due to gravity.
+
+	public:
+		size_t m_update_count;
+		float m_restitution;             // Coefficient of restitution applied in collision response.
+		bool m_apply_collision_response; // Whether to apply collision response or not.
+		bool m_bool_apply_kinematic;     // Whether to apply kinematic equations or not.
+
+		PhysicsSystem(SceneSystem& scene_system, CollisionSystem& collision_system);
+		void integrate(const DeltaTime& delta_time);
+	private:
+		void init_jolt();
 	};
 } // namespace System
